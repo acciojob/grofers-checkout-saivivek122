3daec3636710b4a7c85e290a6de7e5d104731522
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-14 09:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const getSumBtn = document.createElement("button");
+let table=document.getElementById("table");
+let prices=document.querySelectorAll(".price")
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let sum=0;
 
 const getSum = () => {
 //Add your code here
+	for(let i=0;i<prices.length;i++){
+	let price=parseInt(prices[i].textContent);
+	sum=sum+price;
+}
+	let tr=table.insertRow();
+	let td1=tr.insertCell(0);
+	let td2=tr.insertCell(1);
+	td1.textContent="total"
+	td2.textContent=sum;
+	
+	
   
 };
 
